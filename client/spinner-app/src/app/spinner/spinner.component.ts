dca89268a837beea59bb1f0dc300b4697a2d30fb
Juan Miguel Choy <juanchoy30@gmail.com>
2020-10-15 19:06:05 -0300

    Spinner Fields sizes and slice form set

--- client/spinner-app/src/app/spinner/spinner.component.ts
+++ client/spinner-app/src/app/spinner/spinner.component.ts
@@ -10,8 +10,11 @@ import { SpinnerService } from '../services/spinner.service';
 export class SpinnerComponent implements OnInit {
 
   SpinnerFields: any;
-  styleExpr: any[] = ['']; // Backgorund color
+  bgColorStyle: any[] = ['']; // Backgorund color
   fontColor: any[] = ['']; // Font Color
+
+  fieldStyleNth: any[] = [''] //Individual field style
+  holder: any[] = ['']
 
   constructor( private spinnerService: SpinnerService) { }
 
@@ -24,13 +27,26 @@ export class SpinnerComponent implements OnInit {
       .subscribe( spinnerFields => {
         //spinnerFields.image = spinnerFields.image.replace("\", "/");
         this.SpinnerFields = spinnerFields;
+        let angle = 360/this.SpinnerFields.length;
         for(let i=0; i<this.SpinnerFields.length; i++) { //Substitute backslashes for slashes
           this.SpinnerFields[i].image = this.SpinnerFields[i].image.replace(/\\/g, "/");
-          this.styleExpr[i] = this.SpinnerFields[i].bgColor; // Backgorund color
-          this.fontColor[i] = this.spinnerService.invertColor(this.styleExpr[i]); //Font Color
+          this.bgColorStyle[i] = this.SpinnerFields[i].bgColor; // Backgorund color
+          this.fontColor[i] = this.spinnerService.invertColor(this.bgColorStyle[i]); //Font Color
+          this.holder[i] = {'transform': 'rotate(' + angle*(i) + 'deg)',
+                            '-webkit-transform': 'rotate(' + angle*(i) + 'deg)',
+                            '-moz-transform': 'rotate(' + angle*(i) + 'deg)',
+                            '-o-transform': 'rotate(' + angle*(i) + 'deg)',
+                            '-ms-transform': 'rotate(' + angle*(i) + 'deg)',}
+
+          this.fieldStyleNth[i] = {'transform': 'rotate(' + angle + 'deg)',
+                                  '-webkit-transform': 'rotate(' + angle + 'deg)',
+                                  '-moz-transform': 'rotate(' + angle + 'deg)',
+                                  '-o-transform': 'rotate(' + angle + 'deg)',
+                                  '-ms-transform': 'rotate(' + angle + 'deg)',
+                                  'background-color': this.bgColorStyle[i],
+                                  'color': this.fontColor[i]	}
         }
-        console.log(this.styleExpr);
-        console.log(this.fontColor);
+        console.log(this.fieldStyleNth);
       })
   }
 
